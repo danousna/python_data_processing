adff2c7dfc6d613f2b203cc91032e64922a3f4b6
Natan Danous <natan.danous@gmail.com>
2019-05-02 11:40:50 +0200

    Generator + prelim data analysis

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # python_data_processing
 
-## Description des données
+## Questions sur les données
 
+### Valeur foncière moyenne sur Compiègne
+
+```sql
+SELECT SUM(Valeur_fonciere)/COUNT(*) FROM table WHERE Valeur_fonciere IS NOT NULL GROUP BY Valeur_fonciere;
+```
+
+### Taille moyenne des terrains.
+
+```sql
+SELECT SUM(Surface_terrain)/COUNT(*) FROM table WHERE Surface_terrain IS NOT NULL GROUP BY Surface_terrain;
+```
+
+### Avoir le nombre de vente sur 1 mois
+
+```sql
+SELECT COUNT(*) FROM table WHERE Nature_mutation = Vente AND MONTH(Date_mutation) = 1 
+```
+
+### Rue où il y a le plus de ventes
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-import csv
+from csv import excel, DictReader
+from os import listdir
+from os.path import isfile, join
+from sys import stderr
 
-with open('compiegne/60159_000AW.csv', newline='') as file:
-    reader = csv.reader(file, delimiter=';')
-    line = 0
-    for row in reader:
-        if line == 1:
-            print(', '.join(row))
-            break
-        line += line + 1
+dial = excel
+dial.delimiter = ';'
+
+def read_file_gen(file_name):
+    with open(file_name) as f:
+        reader = DictReader(f, dialect=dial)
+        for r in reader:
+            yield dict(r)
+
+files = [f for f in listdir('compiegne/') if isfile(join('compiegne/', f))]
+count = 0
+parcelles = {}
+
+for file_name in files:
+    gen = read_file_gen(join('compiegne/', file_name))
+    for item in gen:
+        count = count + 1
+        if item['Code parcelle'] in parcelles.keys():
+            parcelles[item['Code parcelle']] = parcelles[item['Code parcelle']] + 1
+        else:
+            parcelles[item['Code parcelle']] = 0
+
+print(count)
+print(parcelles)
+
